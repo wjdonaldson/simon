2e516863ef5fbe8816b815b0a504bde54d267435
Jay <jay@wjdonaldson.com>
2023-06-26 21:40:25 -0700

    refactor code to use const SPACES object

--- js/main.js
+++ js/main.js
@@ -1,16 +1,42 @@
 // Simon Game
 
 /*----- constants -----*/
+const SPACES = {
+  tl: {
+    num: 1,
+    offColor: "rgba(255, 255, 0, 0.5)",
+    onColor: "rgba(255, 255, 0, 1)",
+    freq: 277.2, // C#,
+  },
+  tr: {
+    num: 2,
+    offColor: "rgba(0, 0, 255, 0.5)",
+    onColor: "rgba(0, 0, 255, 1)",
+    freq: 392.0, // G;
+  },
+  bl: {
+    num: 2,
+    offColor: "rgba(255, 0, 0, 0.5)",
+    onColor: "rgba(255, 0, 0, 1)",
+    freq: 329.6, // E;
+  },
+  br: {
+    num: 2,
+    offColor: "rgba(0, 128, 0, 0.5)",
+    onColor: "rgba(0, 128, 0, 1)",
+    freq: 784.0, // G (up octive);
+  },
+};
 
 /*----- state variables -----*/
 let sequence = [1];
 
 /*----- cached elements  -----*/
-const yellowEl = document.getElementById("tl");
-console.log(yellowEl);
-const blueEl = document.getElementById("tr");
-const redEl = document.getElementById("bl");
-const greenEl = document.getElementById("br");
+const board = document.getElementById("board");
+const tlEl = document.getElementById("tl");
+const trEl = document.getElementById("tr");
+const blEl = document.getElementById("bl");
+const brEl = document.getElementById("br");
 
 const ctx = new AudioContext();
 var osc = null;
@@ -24,77 +50,49 @@ init();
 
 function init() {
   console.log("Initializing");
-  yellowEl.addEventListener("click", onClickYellow);
-  blueEl.addEventListener("click", onClickBlue);
-  redEl.addEventListener("click", onClickRed);
-  greenEl.addEventListener("click", onClickGreen);
+  board.addEventListener("click", onClickSpace);
 
   // renderSequence();
 }
 
-function renderSequence() {
-  renderYellow();
-  renderBlue();
-  renderRed();
-  renderGreen();
+function renderSequence() {}
+
+function renderSpace(el) {
+  console.log("renderSpace");
+  console.log(el);
+  const space = el.getAttribute("id");
+  console.log(space);
+  el.style.backgroundColor = SPACES[space].onColor;
 }
 
-function renderYellow() {
-  console.log("renderYellow");
-  yellowEl.style.backgroundColor = "rgba(255, 255, 0, 1)";
-}
-function renderBlue() {
-  console.log("renderBlue");
-  blueEl.style.backgroundColor = "rgba(0, 0, 255, 1)";
-}
-
-function renderRed() {
-  console.log("renderRed");
-  redEl.style.backgroundColor = "rgba(255, 0, 0, 1)";
-}
-
-function renderGreen() {
-  console.log("renderGreen");
-  greenEl.style.backgroundColor = "rgba(0, 128, 0, 1)";
-}
-
-function onClickYellow() {
-  renderYellow();
-  playTone(277.183);
-}
-
-function onClickBlue() {
-  renderBlue();
-  playTone(391.995);
-}
-
-function onClickRed() {
-  renderRed();
-  playTone(329.628);
-}
-
-function onClickGreen() {
-  renderGreen();
-  playTone(783.991);
+function onClickSpace(evt) {
+  renderSpace(evt.target);
+  const space = evt.target.getAttribute("id");
+  playTone(SPACES[space].freq);
 }
 
 function playTone(hertz) {
   if (playing) {
     console.log("already playing");
+    // TODO: if already playing, stop the current tone
+    // before playing the new one - NOT WORKING!
+    // gain.gain.exponentialRampToValueAtTime(0.00001, ctx.currentTime + 0.001);
+    // playing = false;
     return;
   }
-  playing = true;
   osc = ctx.createOscillator();
   gain = ctx.createGain();
   osc.connect(gain);
   gain.connect(ctx.destination);
   osc.type = "sine";
-  // osc.frequency.setValueAtTime(440, ctx.currentTime); // value in hertz
   osc.frequency.value = hertz; // value in hertz
+  playing = true;
   osc.start();
   setTimeout(function () {
     console.log("callback");
-    gain.gain.exponentialRampToValueAtTime(0.00001, ctx.currentTime + 1);
-    playing = false;
+    gain.gain.exponentialRampToValueAtTime(0.00001, ctx.currentTime + 0.5);
+    setTimeout(function () {
+      playing = false;
+    }, 500);
   }, 1000);
 }
