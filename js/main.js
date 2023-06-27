74bc14ef390050b611774138eaaf65ae46d7ea2b
Jay <jay@wjdonaldson.com>
2023-06-27 16:09:57 -0700

    add javascript logic for game play

--- js/main.js
+++ js/main.js
@@ -28,19 +28,22 @@ const SPACES = {
   },
 };
 const TIME_BETWEEN_SPACES = 1000; // in ms
+const TIME_BETWEEN_SEQ = 1000;
 const TIME_TO_PLAY_TONE = 500; // in ms
 const TIME_RAMP_DOWN = 0.1; // in seconds
 const WAIT_FOR_RAMP_DOWN = 110; // in ms
 
 /*----- state variables -----*/
-let sequence = [4, 3, 2, 1, 1];
-var playingSeq = false; // Is the computer currently playing a sequence?
+const simonSequence = [];
+const playerSequence = [];
+var playingSeq = null; // Is Simon currently playing a sequence?
 
 /*----- cached elements  -----*/
 const board = document.getElementById("board");
 const startBtn = document.getElementById("game-start-btn");
 const seqLen = document.getElementById("game-seq-len-val");
 const seqLeft = document.getElementById("game-seq-left-val");
+const statusMsg = document.getElementById("game-message");
 
 /*-- The audio cached elements --*/
 const ctx = new AudioContext();
@@ -57,24 +60,31 @@ init();
 
 function init() {
   console.log("Initializing");
+  simonSequence.length = 0;
+  playerSequence.length = 0;
+  startBtn.disabled = false;
+  playingSeq = false;
+  playingTone = false;
+  statusMsg.innerText = "";
 }
 
 // plays one element of the sequence, then recursively calls
 // itself to play the next element in the sequence.
 function playSequence(idxToPlay) {
   if (idxToPlay === 0) {
-    seqLen.innerText = sequence.length;
+    seqLen.innerText = simonSequence.length;
   }
-  seqLeft.innerText = sequence.length - idxToPlay;
+  seqLeft.innerText = simonSequence.length - idxToPlay;
   console.log(`playSequence(${idxToPlay})`);
-  if (idxToPlay === sequence.length) {
+  if (idxToPlay === simonSequence.length) {
     playingSeq = false;
+    statusMsg.innerText = "Your Turn";
     console.log("Done playing sequence");
     return;
   }
   for (let space in SPACES) {
-    if (SPACES[space].num === sequence[idxToPlay]) {
-      console.log(`iterate, ${sequence[idxToPlay]}`);
+    if (SPACES[space].num === simonSequence[idxToPlay]) {
+      // console.log(`iterate, ${simonSequence[idxToPlay]}`);
       playingSeq = true;
       const el = document.getElementById(space);
       renderSpace(el);
@@ -86,7 +96,7 @@ function playSequence(idxToPlay) {
 }
 
 function renderSpace(el) {
-  console.log("renderSpace");
+  // console.log("renderSpace");
   const space = el.getAttribute("id");
   el.style.backgroundColor = SPACES[space].onColor;
   playTone(SPACES[space].freq);
@@ -96,13 +106,53 @@ function renderSpace(el) {
 }
 
 function onClickSpace(evt) {
+  if (playingSeq || playingTone) return;
   renderSpace(evt.target);
   const space = evt.target.getAttribute("id");
-  playTone(SPACES[space].freq);
+  playerSequence.push(SPACES[space].num);
+  checkCorrect();
 }
 
 function onClickStart(evt) {
+  startSimonSequence();
+}
+
+function startSimonSequence() {
+  startBtn.disabled = true;
+  startBtn.style.visibility = "hidden";
+  statusMsg.innerText = "My Turn";
+  simonSequence.push(generateRandonSpace());
   playSequence(0);
+}
+
+function startPlayerSequence() {
+  playerSequence.length = 0;
+  // TODO: enable space clicks?
+}
+
+function generateRandonSpace() {
+  const spaceNum = Math.floor(Math.random() * 4) + 1;
+  console.log(`random space: ${spaceNum}`);
+  return spaceNum;
+}
+
+function checkCorrect() {
+  const arr1 = playerSequence;
+  const arr2 = simonSequence.slice(0, playerSequence.length);
+  if (arr1.every((value, index) => value === arr2[index])) {
+    if (playerSequence.length === simonSequence.length) {
+      playerSequence.length = 0;
+      setTimeout(function () {
+        startSimonSequence();
+      }, TIME_BETWEEN_SPACES + TIME_BETWEEN_SEQ);
+    }
+  } else {
+    init();
+    statusMsg.innerText = "Game Over";
+    startBtn.innerText = "Start Over";
+    startBtn.disabled = false;
+    startBtn.style.visibility = "visible";
+  }
 }
 
 function playTone(hertz) {
@@ -123,7 +173,7 @@ function playTone(hertz) {
   playingTone = true;
   osc.start();
   setTimeout(function () {
-    console.log("callback");
+    // console.log("callback");
     gain.gain.exponentialRampToValueAtTime(
       0.00001,
       ctx.currentTime + TIME_RAMP_DOWN
